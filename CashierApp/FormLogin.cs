4762b2b3eb965661b4af6ff8181c53493d67f5e4
Miftah Apriliandi <miftahhatfim2304@gmail.com>
2021-09-19 21:36:45 +0700

    Initial Commit

---
+++ CashierApp/FormLogin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashierApp
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "KSR001" && textBox2.Text == "12345")
+            {
+                FormHome frmHome = new FormHome();
+                frmHome.Show();
+            }
+            else
+            {
+                MessageBox.Show("Kode / Password Anda salah!");
+            }
+        }
+    }
+}
